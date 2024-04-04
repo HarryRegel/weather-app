2a0a1eb760dfa5620b8c79a892af6a99f03e0f49
Krypto-etox <161178212+Krypto-etox@users.noreply.github.com>
2024-04-04 21:05:34 +0530

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "WeatherApp"
+include(":app")
+include(":shared")
