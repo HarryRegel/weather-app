ce0e7580f77dfdfbdcdfcb78a37eeab96b7b0e2c
Harry Regel <131510131+HarryRegel@users.noreply.github.com>
2024-04-04 07:50:33 -0700

    Add files via upload

---
+++ iOSApp/iOSApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  iOSApp
+//
+//  Created by David Odari Kiribwa on 07.03.24.
+//
+
+import SwiftUI
+import shared
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint) }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
